62caf14b7da80672a19e62f3f86813a0512d3883
Twilight <cuccubird02@gmail.com>
2023-07-10 07:13:44 +0700

    first-setup-done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './index.css';
+import Main from './page/Main';
 
 function App() {
   return (
     <div className="App">
-      
+      <Main/>
     </div>
   );
 }
---
+++ src/page/Main.js
@@ -0,0 +1,7 @@
+export default function Main(props) {
+    return (
+        <>
+         <div className="min-h-screen bg-black"></div>
+        </>
+    )
+}
